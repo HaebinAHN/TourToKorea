133e44fe2885d54f2dea4603280bb358f11b8637
destiny3912 <destiny3910@gmail.com>
2022-05-09 16:05:41 +0900

    FIX : Google Login Problem

--- app/src/main/java/com/teamb/tourtokorea/MainActivity.java
+++ app/src/main/java/com/teamb/tourtokorea/MainActivity.java
@@ -17,10 +17,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+/**
+ * 로그인 페이지를 mainActivity로 사용합니다.
+ * */
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
+    private FirebaseAuth mAuth = null;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+    private SignInButton signInButton;
+
     String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     @Override
@@ -28,7 +50,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 임시 기능 구현 예제용 예시들, 인텐트 호출 형식 확인바람.
+        //로그인 버튼과 파이어베이스 인증 instance를 받아옵니다.
+        signInButton = findViewById(R.id.signInButton);
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+       /*// 임시 기능 구현 예제용 예시들, 인텐트 호출 형식 확인바람.
 
         // 퍼미션 확인 후 퍼미션 권한 요청.
         if (checkLocationServicesStatus()) {
@@ -83,10 +121,72 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }
-        });
+        });*/
     }
 
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+
+            case GPS_ENABLE_REQUEST_CODE:
+
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+            case RC_SIGN_IN:
+
+                //로그인
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try{
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(account);
+                }catch (ApiException exception){
+
+                }
+                break;
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        }else {
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+        if(user != null) {
+
+            Intent intent = new Intent(this, Selectpage.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     //하부 메소드들 : GPS 신호 사용을 위한 권한설정 메소드 (퍼다구함)
     @Override
     public void onRequestPermissionsResult(int permsRequestCode,
@@ -203,27 +303,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode) {
-
-            case GPS_ENABLE_REQUEST_CODE:
-
-                //사용자가 GPS 활성 시켰는지 검사
-                if (checkLocationServicesStatus()) {
-                    if (checkLocationServicesStatus()) {
-
-                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
-                        checkRunTimePermission();
-                        return;
-                    }
-                }
-
-                break;
-        }
-    }
 
     public boolean checkLocationServicesStatus() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
--- app/src/main/java/com/teamb/tourtokorea/Selectpage.java
+++ app/src/main/java/com/teamb/tourtokorea/Selectpage.java
@@ -3,12 +3,40 @@ package com.teamb.tourtokorea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Selectpage extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    Button logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectpage);
+
+        logoutBtn = (Button)findViewById(R.id.logoutButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logoutBtn.setOnClickListener(view -> {
+            signOut();
+            finishAffinity();
+        });
+    }
+    /**
+     * 로그아웃 메서드
+     * */
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+    }
+    
+    /**
+     * 회원탈퇴 메서드
+     * */
+    private void revokeAccess() {
+        mAuth.getCurrentUser().delete();
     }
 }
